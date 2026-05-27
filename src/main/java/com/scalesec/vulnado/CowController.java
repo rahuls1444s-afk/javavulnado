a5d3f9a3455f289d47db4c1823369b3fae06bf72
rahuls1444s-afk <rahuls1444s@gmail.com>
2026-05-27 12:27:28 +0530

    Update CowController.java

--- src/main/java/com/scalesec/vulnado/CowController.java
+++ src/main/java/com/scalesec/vulnado/CowController.java
@@ -2,7 +2,7 @@ package com.scalesec.vulnado;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.autoconfigure.*;
-import org.apache.commons.*;
+inport org.springframework.web.util.HtmlUtils;
 import java.io.Serializable;
 
 @RestController
@@ -10,7 +10,7 @@ import java.io.Serializable;
 public class CowController {
     @RequestMapping(value = "/cowsay")
     String cowsay(@RequestParam(defaultValue = "I love Linux!") String input) {
-        String vInput = StringEscapeUtils.escapeHtml4(input);
+        String vInput = HtmlUtils.htmlEscape(input);
         return Cowsay.run(vInput);
     }
 }
